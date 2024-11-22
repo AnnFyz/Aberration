ae8ed64c435ea3bc104665cfca925058265a9ca5
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2024-11-22 19:24:19 +0100

    add

--- Aberration/Assets/Scripts/Interactables/PlatformInteractable.cs
+++ Aberration/Assets/Scripts/Interactables/PlatformInteractable.cs
@@ -107,8 +107,8 @@ public class PlatformInteractable : XRBaseInteractable
     {
         base.OnSelectEntered(args);
 
-        handleToHandLine.gameObject.SetActive(false);
-        dragVectorLine.gameObject.SetActive(false);
+        handleToHandLine.gameObject.SetActive(true);
+        dragVectorLine.gameObject.SetActive(true);
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)

28f8e8cea4b255c13b7b60f9a7f784f781dbb1d4
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2024-12-11 19:02:19 +0100

    add

--- Aberration/Assets/Scripts/GrenadeThrower.cs
+++ Aberration/Assets/Scripts/GrenadeThrower.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors.Visuals;
 
 public class GrenadeThrower : MonoBehaviour
 {
-    
+    [SerializeField] Transform grenadePrefab;
+    [SerializeField] float throwForce = 40f;
+    [SerializeField] CurveVisualController visualController;
+
+    private void Awake()
+    {
+        visualController = GetComponent<CurveVisualController>();
+    }
+
+    public void ThrowGrenade()
+    {
+
+    }
 }
